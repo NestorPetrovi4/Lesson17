3d5e82a8cad33999bcba13a13acdd3990e6366af
Oleg Nesterov <oleg9988@rambler.ru>
2022-09-13 11:59:43 +0300

    Добавлена функция получения списка посетителей

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        
+
     }
+
+    public static List<Person> generateClients() {
+        List<Person> persons = new ArrayList<>();
+        persons.add(new Person("Oleg", "Nesterov", 3));
+        persons.add(new Person("Vitaliy", "Zuev", 1));
+        persons.add(new Person("Polina", "Markova", 5));
+        persons.add(new Person("Andrey", "Matveev", 2));
+        persons.add(new Person("Viktoriya", "Karelskaya", 2));
+        return persons;
+    }
+
 }
