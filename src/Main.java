b0bfa8bc808e7149902c7ca8d6f9b92d667c5632
Oleg Nesterov <oleg9988@rambler.ru>
2022-09-13 14:08:43 +0300

    Добавлена работа с очередью

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,29 @@
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        List<Person> persons = generateClients();
+        Deque<Person> dequePerson = new ArrayDeque<>();
+        dequePerson.addAll(persons);
+        while (true) {
+            Person person = dequePerson.poll();
+            if (person == null) {
+                System.out.print("Очередь закончилась!!!");
+                break;
+            }
+            int amountTicket = person.getAmountTicket();
+            if (amountTicket - 1 > 0) {
+                System.out.println(person.name + " " + person.surname + " прокатился на аттракционе осталось билетов " + (amountTicket - 1));
+                person.setAmountTicket(amountTicket - 1);
+                dequePerson.addLast(person);
+            } else {
+                System.out.println(person.name + " " + person.surname + " прокатился на аттракционе");
+            }
+
+        }
 
     }
 
